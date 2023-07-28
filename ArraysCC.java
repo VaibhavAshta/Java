d997b9bd80e0f29d979c0662ac1580b01eb453b7
VaibhavAshta <88577836+VaibhavAshta@users.noreply.github.com>
2023-07-29 03:55:10 +0530

    Update ArraysCC.java
    added code for linearSearch()

--- ArraysCC.java
+++ ArraysCC.java
@@ -21,3 +21,56 @@ public class ArraysCC {
 
     }
 }
+
+import java.util.*;
+public class ArraysCC {
+
+    public static int linearSearch(int numbers[], int key){
+
+        for(int i=0; i<numbers.length; i++){
+            if( numbers[i] == key){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+    public static void main(String args[]){
+        int numbers[] = {1, 2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 0;
+
+        int index = linearSearch(numbers, key);
+        if(index == -1){
+            System.out.print("Key Not Found");
+        } else{
+            System.out.print(" Key found at index: "+index);
+        }
+
+    }
+
+}
+
+public class ArraysCC {
+
+    public static int linearSearch(String menu[], String key) {
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1; // Element not found, return -1.
+    }
+
+    public static void main(String args[]) {
+        String menu[] = {"Dosa", "Chole Bhature", "Samosa", "Pizza", "Vada Pav"};
+        String key = "Samosa";
+
+        int index = linearSearch(menu, key);
+        if (index == -1) {
+            System.out.print("Key Not Found");
+        } else {
+            System.out.print(" Key found at index: " + index);
+        }
+    }
+}
